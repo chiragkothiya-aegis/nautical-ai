6f89f15811cb7b2143ddcf6c71d363ea8fbc254c
chiragkothiya-aegis <chirag.kothiya@aegisiscteam.com>
2024-03-26 10:57:53 +0530

    Sign changes done

--- src/screens/login/Signup.tsx
+++ src/screens/login/Signup.tsx
@@ -59,10 +59,18 @@ function Signup() {
     <FormInput name={"lastName"} placeholder="Last Name" isRequired />,
     <FormInput
       name={"organisation_institution"}
-      placeholder="organisation/Institution"
+      placeholder="Organisation/Institution"
       isRequired
     />,
-    <FormInput name={"country"} placeholder="Country" isRequired />,
+    // <FormInput name={"country"} placeholder="Country" isRequired />,
+    <FormSelect
+      name={"country"}
+      placeholder="Country"
+      options={[
+        { value: "usa", label: "USA" },
+        { value: "australia", label: "Australia" },
+      ]}
+    />,
     <FormInput name={"phone Number"} placeholder="Phone Number" isRequired />,
     <FormInput name={"email"} type="email" placeholder="Email" isRequired />,
     <FormInput
@@ -80,7 +88,15 @@ function Signup() {
     <FormSelect
       name={"iAm"}
       label="I am a"
-      options={[{ value: "seafaser", label: "seafaser" }]}
+      options={[
+        { value: "student", label: "Student" },
+        { value: "seafarer", label: "Seafarer" },
+        {
+          value: "shore_based_professional",
+          label: "Shore-based professional",
+        },
+        { value: "other", label: "Other" },
+      ]}
     />,
   ];
 
